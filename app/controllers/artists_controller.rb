b795833094f906d518cc95a7b78fe181a5a64920
Max Morton <max.morton.42@gmail.com>
2019-11-17 21:53:58 -0700

    functionfinish

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,6 @@
 class ArtistsController < ApplicationController
   before_action :set_billboard
-  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+  before_action :set_artist, only: [:show, :edit, :update, :destroy, :index]
  
   
   def index
